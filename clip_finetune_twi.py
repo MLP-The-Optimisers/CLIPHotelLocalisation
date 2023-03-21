a4c3060965eb22855ea7cd298685b95158b8b0f9
Abdul Rehman <arehmannasir@outlook.com>
2023-03-21 22:18:36 +0000

    fixed test array size

--- clip_finetune_twi.py
+++ clip_finetune_twi.py
@@ -247,8 +247,8 @@ if __name__ == "__main__":
         # Load and split dataset
         images, labels = load_dataset_pairs()
         images_train, images_val, labels_train, labels_val = train_test_split(
-            images[364], 
-            labels[364], 
+            images[0:1000], 
+            labels[0:1000], 
             test_size=0.20, 
             random_state=42
         )
