1cf8cd8634b733a99d059f11fad8fe238857c06f
Abdul Rehman <arehmannasir@outlook.com>
2023-03-21 21:54:50 +0000

    s

--- clip_finetune_twi.py
+++ clip_finetune_twi.py
@@ -247,11 +247,12 @@ if __name__ == "__main__":
         # Load and split dataset
         images, labels = load_dataset_pairs()
         images_train, images_val, labels_train, labels_val = train_test_split(
-            images[1000], 
-            labels[1000], 
+            images[364], 
+            labels[364], 
             test_size=0.20, 
             random_state=42
         )
+
 
         console.log("Dataset loaded and split successfully!")
         status.update(status=f'Creating dataset loading classes..')
