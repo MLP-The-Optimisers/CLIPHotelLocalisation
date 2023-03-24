02f2c5c96ec3636fad9e40792c351f7838f77823
Abdul Rehman <arehmannasir@outlook.com>
2023-03-24 05:43:14 +0000

    latest changes

--- src/eval_occluded.py
+++ src/eval_occluded.py
@@ -28,10 +28,11 @@ class Eval():
 
         self.df_train = pd.read_csv("data/input/dataset/train_set.csv")
         self.df_hotels = pd.read_csv("data/input/dataset/hotel_info.csv")
+        self.df_test = pd.read_csv("data/input/dataset/test_set.csv")
 
 
     def _list_test_images(self, repo: str) -> List[str]:
-        return glob.glob(repo, recursive=True)
+        return glob.glob(repo, recursive=False)
     
 
     def _parse_img_id(self, image_path: str) -> int:
@@ -54,17 +55,23 @@ class Eval():
         for image_path in track(self._list_test_images(test_repo), description=f"Processing latents..."):
             latents.append((self._parse_img_id(image_path), self._img_to_latent(image_path)))
         
-        save_object(latents, 'test_latents', 'latents')
+        save_object(latents, 'test_latents_unoccluded', 'latents')
 
     def top_k_acc_instance(self, ks: List, test_repo: str) -> float:
         results = {f'top-{k}': 0 for k in ks}
         max_k = max(ks)
-        test_latents = load_object('test_latents', 'latents')
+        test_latents = load_object('test_latents_unoccluded', 'latents')[0:1000]
+        not_present = 0
 
         for gold_id, gold_latent in track(test_latents, description=f"processing up to top {max_k} instance"):
             _, ids = self.index.search_cosine(gold_latent, top_k=max_k)
-            gold_hotel_id = self.df_train.loc[
-                self.df_train['image_id'] == gold_id]['hotel_id'].iloc[0]
+            try:
+                gold_hotel_id = self.df_test.loc[
+                    self.df_test['image_id'] == gold_id]['hotel_id'].iloc[0]
+            except:
+                print("image {} not found in the fucking test set. FUCK YOU HOTELS 50K!!!!".format(gold_id))
+                not_present += 1
+                continue
 
             for idx, i in enumerate(ids[0]):
                 hotel_id = self.df_train.loc[self.df_train['image_id'] == i]['hotel_id'].iloc[0]
@@ -74,7 +81,7 @@ class Eval():
                             results[f'top-{k}'] += 1
                     break
 
-        total = len(test_latents)
+        total = len(test_latents) - not_present
         for k in ks:
             results[f'top-{k}'] /= total
 
@@ -88,12 +95,18 @@ class Eval():
     def top_k_acc_chain(self, ks: List, test_repo: str) -> float:
         results = {f'top-{k}': 0 for k in ks}
         max_k = max(ks)
-        test_latents = load_object('test_latents', 'latents')
+        test_latents = load_object('test_latents_occluded', 'latents')[0:10000]
+        not_present = 0
 
         for gold_id, gold_latent in track(test_latents, description=f"processing up to top {max_k} chain"):
             _, ids = self.index.search_cosine(gold_latent, top_k=max_k)
-            gold_hotel_id = self.df_train.loc[
-                self.df_train['image_id'] == gold_id]['hotel_id'].iloc[0]
+            try:
+                gold_hotel_id = self.df_test.loc[
+                    self.df_test['image_id'] == gold_id]['hotel_id'].iloc[0]
+            except:
+                print("image {} not found in the fucking test set. FUCK YOU HOTELS 50K!!!!".format(gold_id))
+                not_present += 1
+                continue
             gold_chain_id = self.df_hotels.loc[
                 self.df_hotels['hotel_id'] == gold_hotel_id]['chain_id'].iloc[0]
 
@@ -109,7 +122,7 @@ class Eval():
                             results[f'top-{k}'] += 1
                     break
 
-        total = len(test_latents)
+        total = len(test_latents) - not_present
         for k in ks:
             results[f'top-{k}'] /= total
 
@@ -122,10 +135,10 @@ class Eval():
 
 if __name__ == '__main__':
     evaluation = Eval('clip_index')
-    evaluation.build_test_latents('data/images/test/*.jpg')
+    # evaluation.build_test_latents('data/images/test/test/unoccluded/*/*/*/*.jpg')
     print("HOTEL CHAIN")
-    print(evaluation.top_k_acc_chain([1, 3, 5], 'data/images/test/*.jpg'))
+    # print(evaluation.top_k_acc_chain([1, 3, 5], 'data/images/test/test/unoccluded/*/*/*/*.jpg'))
     print()
 
     print("HOTEL INSTANCE")
-    print(evaluation.top_k_acc_instance([1, 10, 100], 'data/images/test/*.jpg'))
+    print(evaluation.top_k_acc_instance([1, 10, 100], 'data/images/test/test/unoccluded/*/*/*/*.jpg'))
